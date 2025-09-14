4039afae26b5c63cea6728996fe73e779eff5033
Ali Syed <alisyed0206@gmail.com>
2025-09-14 11:32:15 -0400

    Initial Commit

---
+++ Swipe-A-Cat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swipe-A-Cat
+//
+//  Created by Ali Syed on 2025-09-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swipe-A-CatTests/Swipe_A_CatTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_A_CatTests.swift
+//  Swipe-A-CatTests
+//
+//  Created by Ali Syed on 2025-09-14.
+//
+
+import Testing
+@testable import Swipe_A_Cat
+
+struct Swipe_A_CatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
