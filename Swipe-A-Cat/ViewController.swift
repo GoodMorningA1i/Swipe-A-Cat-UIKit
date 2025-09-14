1caad3cf2714163aff55fb3d28141171480d43d6
Ali Syed <alisyed0206@gmail.com>
2025-09-14 11:50:54 -0400

    got a working api call

--- Swipe-A-Cat/ViewController.swift
+++ Swipe-A-Cat/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var catURL: UILabel!
+    
+    struct Cat: Decodable {
+        let id: String
+        let url: String
+        let width: Int
+        let height: Int
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Task {
+            await fetchData()
+        }
+    }
+    
+    private func fetchData() async {
+        guard let url = URL(string: "https://api.thecatapi.com/v1/images/search") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decoded = try JSONDecoder().decode([Cat].self, from: data)
+            print(decoded[0].url)
+        } catch {
+            print("error")
+        }
     }
 
 
